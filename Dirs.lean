e7ac287960b309cb0b4d3beede857b682f8b057d
Xubai Wang <self@xubai.wang>
2022-07-02 16:25:09 +0800

    feat: add support for macos

--- Dirs.lean
+++ Dirs.lean
@@ -1,83 +1,40 @@
-open System
+import Lean
+import Dirs.Linux
+import Dirs.MacOS
+
+open System Lean
 
 -- TODO: non unix system
 
 namespace Dirs
 
-def getDirFromEnv? (s : String) (exts : List FilePath := []) : BaseIO $ Option FilePath := do
-  if let some d ← IO.getEnv s then
-    let d := exts.foldl (λ acc x => acc.join x) (FilePath.mk d)
-    return if ←d.isDir then some d else none
-  else return none
+macro "platform_def" n:ident : command =>
+  let n' := n.getId
+  `(
+    def $n : BaseIO $ Option FilePath :=
+      if System.Platform.isOSX then
+        $(mkIdent (Name.append `Dirs.MacOS n'))
+      else
+        $(mkIdent (Name.append `Dirs.Linux n'))
+  )
 
-def home : BaseIO $ Option FilePath :=
-  getDirFromEnv? "HOME"
-
-def audio : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_MUSIC_DIR"
-
-def cache : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_CACHE_HOME" with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".cache"]
-
-def config : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_CONFIG_HOME" with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".config"]
-
-def data : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_DATA_HOME" with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".local", "share"]
-
-def dataLocal : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_DATA_HOME" with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".local", "share"]
-
-def desktop : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_DESKTOP_DIR"
-
-def document : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_DOCUMENTS_DIR"
-
-def download : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_DOWNLOAD_DIR"
-
-def executable : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_BIN_HOME" with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".local", "bin"]
-
-def font : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_BIN_HOME" ["fonts"] with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".local", "share", "fonts"]
-
-def picture : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_PICTURES_DIR"
-
-def preference : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_CONFIG_HOME" ["fonts"] with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".config"]
-
-def public : BaseIO $ Option FilePath := 
-  getDirFromEnv? "XDG_PUBLICSHARE_DIR"
-
-def runtime : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_RUNTIME_DIR"
-
-def state : BaseIO $ Option FilePath := do
-  match ←getDirFromEnv? "XDG_STATE_HOME" ["fonts"] with
-  | some d => return some d
-  | _ => getDirFromEnv? "HOME" [".local", "state"]
-
-def template : BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_TEMPLATES_DIR"
-
-def video: BaseIO $ Option FilePath :=
-  getDirFromEnv? "XDG_VIDEOS_DIR"
+platform_def home
+platform_def audio
+platform_def cache
+platform_def config
+platform_def data
+platform_def dataLocal
+platform_def desktop
+platform_def document
+platform_def download
+platform_def executable
+platform_def font
+platform_def picture
+platform_def preference
+platform_def public
+platform_def runtime
+platform_def state
+platform_def template
+platform_def video
 
 end Dirs
